b624136d0fef90aad81990775b4b71aa7ab061f2
Hasinipasupureddi <hasini.dsl.pasupureddi@gmail.com>
2025-04-05 12:52:23 +0530

    Create elif.py

---
+++ elif.py
@@ -0,0 +1,11 @@
+marks=int(input())
+if(marks>=90):              
+    print("grade a")
+elif(marks>=75):
+    print("grade b")
+elif(marks>=55):
+    print("grade c")
+elif(marks>=33):
+    print("grade d")
+else:
+    print("fail")
